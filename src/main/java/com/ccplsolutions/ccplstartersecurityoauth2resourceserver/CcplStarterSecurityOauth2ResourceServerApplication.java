ff36cc994b1fccb45d625353a3b186c0fd42cb45
pedro-chavezcastro <pedro.chavezcastro@outlook.com>
2023-06-08 18:45:32 -0500

    first commit

---
+++ src/main/java/com/ccplsolutions/ccplstartersecurityoauth2resourceserver/CcplStarterSecurityOauth2ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.ccplsolutions.ccplstartersecurityoauth2resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcplStarterSecurityOauth2ResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcplStarterSecurityOauth2ResourceServerApplication.class, args);
+	}
+
+}
